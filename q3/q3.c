7db2b56f3e9627fde953500c5545dc15023359f9
sharktron28 <up202108721@edu.fe.up.pt>
2022-11-22 17:07:58 +0000

    q3 working (missing stuff)

--- q3/q3.c
+++ q3/q3.c
@@ -30,39 +30,56 @@ int main(int argc, char* argv[]) {
         printf("usage: n p t\n");
         return -1;
     }
-    create_pipes(*argv[2]);
-    int temp;
+    float p = atof(argv[2]);
+    int n = atoi(argv[1]);
+    create_pipes(*argv[1]);
     pid_t pid;
-    for(int y = 1; y <= *argv[2] - '0'; y++){
+    for(int y = 1; y <= *argv[1] - '0'; y++){
         pid_t pid1 = fork();
         pid = pid1;
-        if(pid1 == 0) {
-            temp = y;
-            break;
+        if(pid == 0){
+            srandom(y);
+            char pipeToRead[] = "pipextoy";
+            char pipeToWrite[] = "pipextoy";
+            if(y == *argv[1] - '0'){
+                pipeToWrite[4] = y + '0';
+                pipeToWrite[7] = '1';
+                pipeToRead[4] = (y - 1) + '0';
+                pipeToRead[7] = y + '0';
+            }
+            else{
+                pipeToWrite[4] = y + '0';
+                pipeToWrite[7] = (y +1) + '0';
+                if(y == 1){
+                    pipeToRead[4] = *argv[1];
+                    pipeToRead[7] = 1 + '0';
+                }
+                else{
+                    pipeToRead[4] = (y - 1) + '0';
+                    pipeToRead[7] = y + '0';
+                }   
+            }
+        int fileDescriptorRead = open(pipeToRead, O_RDONLY);
+        int fileDescriptorWrite = open(pipeToWrite, O_WRONLY);
+        int number, size = 1;
+        sleep(1);
+        while((size = read(fileDescriptorRead, &number, sizeof(int))) > 0){
+            if(random() % 100 < p * 100){
+                printf("lock on token (val = %d)\n", number);
+                sleep(*argv[3] - '0');
+                printf("unlock token\n");
+            }
+            number++;
+            write(fileDescriptorWrite, &number, sizeof(int));
         }
+        printf("reached this %d\n", y);
+        return 0;
     }
-    if(pid == 0){
-    char pipeToRead[] = "pipextoy";
-    char pipeToWrite[] = "pipextoy";
-    if(temp == *argv[2] - '0'){
-        pipeToWrite[4] = temp + '0';
-        pipeToWrite[7] = '1';
-        pipeToRead[4] = (temp - 1) + '0';
-        pipeToRead[7] = temp + '0';
-    }
-    else{
-        pipeToWrite[4] = temp + '0';
-        pipeToWrite[7] = (temp +1) + '0';
-        pipeToRead[4] = (temp - 1) + '0';
-        pipeToRead[7] = temp + '0';   
-    }
-    printf("id: %d;\ntoWrite: %s;\ntoRead: %s;\n", getpid(), pipeToWrite, pipeToRead);
-    return 0;
-    }
-    wait(NULL);
-    return 0;
-    /*while(read(antecessor)){
-        ... (check a chance)
-        write(sucessor)
-    }*/
+}
+int fileDescriptor = open("pipe1to2", O_WRONLY); 
+int first = 0;
+write(fileDescriptor, &first, sizeof(int));
+close(fileDescriptor);
+wait(NULL);
+return 0;
 }
